57428b86ca8cb4b6db421f1ebf0ed3ffd8c8e23a
Ethan Skinner <10249884+el-ethan@users.noreply.github.com>
2022-12-26 14:53:31 -0600

    Finish v1 of guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,37 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
-
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
-
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    loop {
+        let mut guess = String::new();
 
-    // TODO: get autoformatting working
+        // TODO: get autoformatting working
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    
-    // "turbofish version": let guess = guess.trim().parse::<u32>()
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+        let guess = match guess.trim().parse::<u32>() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!")
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
